9e68c980ac21447551a64b3027f652e14595cb87
BhuwanDeveloper <khatribhuwan055@gmail.com>
2024-08-23 12:48:56 +1000

    react

--- product_search_app/src/components/FilterableProductTable.jsx
+++ product_search_app/src/components/FilterableProductTable.jsx
@@ -1,11 +1,36 @@
+import {useState} from "react";
 import ProductTable from "./ProductTable";
 import SearchBar from "./SearchBar";
 
 function FilterableProductTable() {
+    const[inStock, setInStock] = useState(false);
+    const [query, setQuery] = useState('');
+    function handleCheckBox(event) {
+        const isChecked = event.target.checked;
+        setInStock(isChecked);
+    }
+    function handleSearch(event) {
+        const searchQuery = event.target.value;
+        setQuery(searchQuery);
+    }
+    console.log(inStock);
+
+    const products = [
+        { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+        { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+        { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+        { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+        { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+        { category: "Electronics", price: "$2000", stocked: true, name: "Iphone" },
+        { category: "Electronics", price: "$2500", stocked: true, name: "Macbook" }
+      ];
+      const filteredProducts = products
+      .filter(product => inStock ? product.stocked: true)
+      .filter(product => product.name.toLowerCase().startsWith(query.toLowerCase()) );
     return (
         <div className="w-96 border">
-            <SearchBar />
-            <ProductTable />
+            <SearchBar handleCheckBox = {handleCheckBox} handleSearch={handleSearch }/>
+            <ProductTable  products={filteredProducts}/>
         </div>
     )
 }
--- product_search_app/src/components/SearchBar.jsx
+++ product_search_app/src/components/SearchBar.jsx
@@ -1,12 +1,14 @@
-function SearchBar() {
+
+function SearchBar({handleCheckBox, handleSearch}) {
     return (
         <form className="p-4">
-            <div><input type="search" className="border text-2xl" placeholder="Search..." /></div>
+            <div style= {{alignContent:"center"}}><input type="search" onChange={handleSearch} className="border text-2xl" placeholder="Search..." /></div>
             <label htmlFor="inStock">
-                <input type="checkbox" name="inStock" id="inStock" />
+                <input onClick={handleCheckBox} type="checkbox" name="inStock" id="inStock" />
                 Only search products in stock
             </label>
         </form>
     );
 }
+
 export default SearchBar;
